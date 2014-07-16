5b74164bb26755e78bcee2add2c802976e115ac0
Joe <usfhussam@live.com>
2014-07-17 00:50:14 +0200

    Added feature to customize fanout again

--- lib/tekeya/entity.rb
+++ lib/tekeya/entity.rb
@@ -12,6 +12,18 @@ module Tekeya
       # default primary key
       define_tekeya_primary_key :id
 
+      # collection to be used when fanning out activities.
+      attr_accessor :fanouts
+      #default fanouts
+      define_tekeya_fanouts :trackers
+
+      def define_tekeya_fanouts(to)
+        fanouts = to
+      end
+
+      def reset_tekeya_fanouts
+        fanouts = trackers
+      end  
 
       # define the relation with the activity
       has_many :activities, as: :entity, class_name: "::Tekeya::Activity", dependent: :destroy do
